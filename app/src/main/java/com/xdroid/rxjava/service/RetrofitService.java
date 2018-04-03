bb756e151d3d7b4e28a3990d1db3373315a977a0
suyimin <suyimin2001@163.com>
2018-04-03 12:38:39 +0800

    增加列表。

--- app/src/main/java/com/xdroid/rxjava/service/RetrofitService.java
+++ app/src/main/java/com/xdroid/rxjava/service/RetrofitService.java
@@ -5,15 +5,6 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-/**
- * 类描述:RetrofitService
- * 创建人:launcher.myemail@gmail.com
- * 创建时间:16-1-24
- * 备注:{@link https://github.com/basil2style/Retrofit-Android-Basic} Thanks for  ,Her code is very good ! I made reference to his code,It saves me a lot of time!
- * 修改人:
- * 修改时间:
- * 修改备注:
- */
 public class RetrofitService {
     private static final String API = "https://api.github.com";
 
